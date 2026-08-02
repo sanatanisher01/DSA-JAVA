236128747174fdefef02d273c203e4ad404f9db6
Aryan Gupta <aryansanatani01@gmail.com>
2026-08-02 15:40:47 +0530

    Update Sheriyans from CodeCollab

--- Automorphic_number.java
+++ Automorphic_number.java
@@ -1,6 +1,27 @@
 // Welcome to CodeCollab!
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Automorphic number
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int square = num * num;
+        int count = 0;
+        int org = num;
+        int mod = 1;
+        while(num>0){
+            count +=1;
+            num = num/10;
+        }
+        for (int i = 0; i<count; i++){
+            mod = mod *10;
+        }
+        int check = square % mod;
+        if(check == org){
+            System.out.print("Automorphic Number");
+        }
+        else{
+            System.out.print("Not a Automorphic Numberr");
+        }
     }
 }
--- Strong_number_check.java
+++ Strong_number_check.java
@@ -2,6 +2,27 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Strong number check
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int ans = 0;
+        int orginal = num;
+        while(num > 0){
+            int digit = num%10;
+            int fact = 1;
+            for(int i = 1; i <= digit; i++){
+                fact = fact * i;
+            }
+            ans = ans + fact;
+            fact = 0;
+            num = num / 10;
+        }
+        if(orginal == ans){
+            System.out.print("Strong Number");
+
+        }
+        else{
+            System.out.print("Not a Strong Number");
+        }
     }
 }
